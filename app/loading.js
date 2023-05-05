9aac7bf02468387103384684de85108fd2c9b7fd
mehmetcelmeli <firtinacelmeli@gmail.com>
2023-05-05 16:06:51 +0300

    update

---
+++ app/loading.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+function Loading () {
+    return (
+        <div className={styles.spinnerWrapper}>
+          <div className={styles.spinner}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      );
+};
+
+export default Loading;
